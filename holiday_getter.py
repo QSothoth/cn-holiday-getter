6faeaabdee781bd24a9922224c1323112faa535f
Sothoth <sossf@qq.com>
2017-11-14 00:00:01 +0800

    Add beijing now

--- holiday_getter.py
+++ holiday_getter.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import urllib2
 import json
+from datetime import datetime, timedelta
 
 
 def get_holiday(year):
@@ -8,9 +9,11 @@ def get_holiday(year):
     :param year: 年份
     :return: 所有放假的日期
     """
-    baidu_calendar_url = 'https://sp0.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php?query={0}年9月' \
+    baidu_calendar_url = 'https://sp0.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php?query={0}年' \
                          '&co=&resource_id=6018&ie=utf8&oe=utf8'.format(year)
     response = urllib2.urlopen(baidu_calendar_url)
+    gmt_format = '%a, %d %b %Y %H:%M:%S GMT'
+    print u'北京时间', datetime.strptime(response.headers['Date'], gmt_format) + timedelta(hours=8)
     holiday_list = json.loads(response.read())['data'][0]['holiday']
 
     # 各个假期放假情况
@@ -36,5 +39,5 @@ def get_holiday(year):
     return off_days
 
 if __name__ == '__main__':
-    # 目前只能获取2017当年的数据，往年的api存在变化
+    # 目前只能获取2017当年的数据，往年的api存在变化，只能按月获取
     get_holiday(u'2017')
